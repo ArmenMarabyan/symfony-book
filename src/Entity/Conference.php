561de6465995791d8e80971912eac5f8a6ca1d83
Fabien Potencier <fabien@potencier.org>
2022-12-19 08:30:16 +0100

    Add 8.4: Creating Entity Classes

---
+++ src/Entity/Conference.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConferenceRepository::class)]
+class Conference
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $year = null;
+
+    #[ORM\Column]
+    private ?bool $isInternational = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function isIsInternational(): ?bool
+    {
+        return $this->isInternational;
+    }
+
+    public function setIsInternational(bool $isInternational): self
+    {
+        $this->isInternational = $isInternational;
+
+        return $this;
+    }
+}

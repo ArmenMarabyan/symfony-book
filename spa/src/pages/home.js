b538f2a78ca928d759fb869c06a2308e832e617d
Armen <armen.marabyan@acra.am>
2023-04-04 17:30:45 +0400

    Created SPA application

---
+++ spa/src/pages/home.js
@@ -0,0 +1,27 @@
+import {h} from 'preact';
+
+
+export default function Home({conferences}) {
+    if (!conferences) {
+        return <div className="p-3 text-center">No conferences yet</div>;
+    }
+    return (
+        <div className="p-3">
+            {conferences.map((conference) => (
+                <div className="card border shadow-sm lift mb-3">
+                    <div className="card-body">
+                        <div className="card-title">
+                            <h4 className="font-weight-light">
+                                {conference.city} {conference.year}
+                            </h4>
+                        </div>
+
+                        <Link className="btn btn-sm btn-primary stretched-link" href={'/conference/' + conference.slug}>
+                            View
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
